c8c8c14fa75751c461493bcc3af55dae679f1515
Falak Tulsi <68032044+Tech13-08@users.noreply.github.com>
2022-12-15 23:59:12 -0800

    Can now be used as a module in code
    Can be pip installed and then imported if the intent is to use within code rather than in command line. Can still be programmed to run with command line inputs however.

--- dijkstra.py
+++ dijkstra.py
@@ -1,13 +1,11 @@
-def createNewDict(prevDist):
+def createNewDict(prevDist, prevName = "", num = -1, data = []):
     global spt, lowestNode, splitString
     dict = {}
-    prevName = input("File Name (or hit enter): ")
-    data = []
-    num = 0
-    if prevName != "":
+    if prevName == "":
+        prevName = input("File Name (or hit enter): ")
         data = open(f"{prevName}.txt", "r").read().splitlines()
         num = int(len(data))
-    else:
+    elif num == -1:
         num = int(input("Number of Nodes: "))
     for i in range(num):
         if prevName == "":
@@ -15,8 +13,8 @@ def createNewDict(prevDist):
             dist = int(input(f"Node {i+1} value: ")) + prevDist
         else:
             name = data[i].split(splitString)[0]
-            dist = int(data[i].split(" in ")[1])+prevDist
-        if name not in spt and dist < 97:
+            dist = int(data[i].split(splitString)[1])+prevDist
+        if name not in spt and dist < stopNum:
             if(name in dict):
                 if dict[name][0] > dist:
                     dict[name] = [dist, {}]
@@ -33,30 +31,42 @@ def findMinNode(dict):
         if(arr[1] != {}):
             findMinNode(arr[1])
         
-def updateNode(dict):
+def updateNode(dict, args):
     global spt, lowestNode, recentMinNum
     spt[lowestNode[0]] = lowestNode[1]
     recentMinNum = lowestNode[1]
-    print(f"Define nodes for {lowestNode[0]}: ")
-    dict[lowestNode[0]][1] = createNewDict(lowestNode[1])
-    print(dict[lowestNode[0]][1])
-    lowestNode = ("", 97)
+    #print(f"Define nodes for {lowestNode[0]}: ")
+    if len(args) == 0:
+        dict[lowestNode[0]][1] = createNewDict(lowestNode[1])
+    else:
+        dict[lowestNode[0]][1] = createNewDict(lowestNode[1], args[0], args[1], args[2])
+    #print(dict[lowestNode[0]][1])
+    lowestNode = ("", stopNum)
 
-def updateMinNode(dict):
+def updateMinNode(dict, args = []):
     global spt, lowestNode, path
     for name, arr in dict.items():
         if(name == lowestNode[0] and arr[0] == lowestNode[1]):
             path.append(name)
-            print(path)
-            updateNode(dict)
+            #print(path)
+            updateNode(dict, args)
             break
         elif(lowestNode[0] == ""):
             break
         elif(arr[1] != {}):
             path.append(name)
-            updateMinNode(arr[1])
+            updateMinNode(arr[1], args)
             if(lowestNode[0] != ""):
                 path.pop()
+def reset(FROM_COMMAND_LINE = False):
+    global spt, recentMinNum, lowestNode, initialNode
+    spt = {}
+    recentMinNum = 0
+    lowestNode = ("", stopNum)
+    if FROM_COMMAND_LINE:
+        initialNode = createNewDict(0) 
+    else:
+        initialNode = {}
 
 spt = {}        # Shortest Path Tree
 recentMinNum = 0        # Holds the most recent lowest node's value
@@ -64,11 +74,12 @@ path = []       # Hold the path to the current lowest node
 splitString = " in "        # String that splits the node names and node values in a text file
 stopNum = 97        # Max distance a node can be from the starting node before the program stops
 lowestNode = ("", stopNum)       # Tuple to hold the lowest node of all currently known nodes
-initialNode = createNewDict(0)      # The initial node with a value of 0
-while(recentMinNum < stopNum):
-    findMinNode(initialNode)
-    updateMinNode(initialNode)
-    path = []
-    print(spt)
+initialNode = {}     # The initial node
 
-
+# This is how to run the algorithm
+# reset()
+# while(recentMinNum < stopNum):
+#     findMinNode(initialNode)
+#     updateMinNode(initialNode)
+#     path = []
+#     print(spt)
