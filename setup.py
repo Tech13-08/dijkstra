2ac4208e0facaf3f0110d7d9c6318eab30799b5f
Falak Tulsi <68032044+Tech13-08@users.noreply.github.com>
2022-12-16 00:02:51 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Tech13-08 Dijkstra',
+    packages=find_packages(),
+    url='https://github.com/Tech13-08/dijkstra',
+    description='This is a description for Tech13-08 Dijkstra',
+    long_description=open('README.md').read(),
+    install_requires=[],
+    dependency_links = [],
+)
