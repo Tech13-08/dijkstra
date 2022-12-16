b0289eb4b14ff946376aa7c79cdd5c54497083c7
Falak Tulsi <68032044+Tech13-08@users.noreply.github.com>
2022-12-16 00:04:54 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Tech13-08 Dijkstra',
+    name='SkizzDijkstra',
     packages=find_packages(),
     url='https://github.com/Tech13-08/dijkstra',
-    description='This is a description for Tech13-08 Dijkstra',
+    description='This is a description for Skizz Dijkstra',
     long_description=open('README.md').read(),
     install_requires=[],
     dependency_links = [],
